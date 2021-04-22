68cf0c6183fd13f7b9b07946002ca079d6c0357e
Lucaswhy <Lucas_herculano_2010@hotmail.com>
2021-04-22 20:03:16 -0300

    ++ dynamics components exercises

---
+++ componentes/comp-avancado-desafio/src/components/Vermelho.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <div class="caixa vermelho">
+        <slot name="conteudo"/>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
